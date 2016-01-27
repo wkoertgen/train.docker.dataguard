53e59262e5d0ee2d9f92d9bf7eda2757dccd52ba
mkoertgen <marcel.koertgen@gmail.com>
2016-01-27 22:00:39 +0100

    refactored docker build

---
+++ build/build_images.sh
@@ -0,0 +1,3 @@
+docker build -f DF_Oracle12c -t oraclelinux:oracle12c . | tee DF_Oracle12c.log
+# docker build -f DF_CreatePrimary -t oraclelinux:primary . | tee DF_CreatePrimary.log
+# docker build -f DF_CreateStandby -t oraclelinux:oracle12c . | tee DF_CreateStandby.log 
--- run.sh
+++
@@ -1,5 +0,0 @@
-docker run -it --name="oracle12c" \
--v $PWD/install:/tmp \
--w /tmp \
-oraclelinux:7.1 \
-/bin/bash -ci ./setup.sh
