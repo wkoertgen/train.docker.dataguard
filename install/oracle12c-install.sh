0ef736e4249621a2cfc711d824b6b44b4f3f3932
HW Körtgen <it@koertgen.de>
2016-01-27 19:29:02 +0100

    Steps for develop with different Dockerfiles

---
+++ install/oracle12c-install.sh
@@ -0,0 +1,53 @@
+chmod 777 /tmp # wg. smonitor.ksh  as oracle
+
+#RUNTIME=$(date +%y%m%d%H%M)
+#LOGFILE=/vagrant/logs/oracle12c-install_$RUNTIME.log
+#echo Oracle12c installation in progress $(date) | tee $LOGFILE
+#echo Logfile is $LOGFILE
+#echo "wait for Oracle12c installation to finish ..."
+
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/12.1.0
+export ORACLE_SID=DEMO
+
+mkdir -p /oracle/install
+chown -R oracle:dba /oracle
+mkdir -p /u01/app
+chown -R oracle:oinstall /u01
+
+su - oracle -c "cp /tmp/oracle12c.rsp /oracle"
+
+unzip /tmp/linuxamd64_12102_database_1of2.zip -d /oracle/install/linuxamd64_12102_database
+unzip /tmp/linuxamd64_12102_database_2of2.zip -d /oracle/install/linuxamd64_12102_database
+
+
+
+
+#run oracle installer
+su - oracle -c "/oracle/install/linuxamd64_12102_database/database/runInstaller \
+-silent -ignoreSysPrereqs -ignorePrereq -noconfig \
+-showProgress -waitforcompletion -responseFile /oracle/oracle12c.rsp"
+
+/u01/app/oraInventory/orainstRoot.sh
+/u01/app/oracle/product/12.1.0/root.sh
+
+errorlevel=$?
+
+if [ "$errorlevel" != "0" ] && [ "$errorlevel" != "6" ]; then
+  echo "There was an error preventing script from continuing"
+  exit 1
+else echo Oracle12c installation finished
+fi
+
+# environment
+cp /tmp/bash_profile /home/oracle/.bash_profile
+cp /tmp/bashrc /home/oracle/.bashrc
+chown oracle:dba /home/oracle/.*
+
+if [ "$errorlevel" != "0" ] && [ "$errorlevel" != "6" ]; then
+  echo "There was an error in setting up environment"
+else echo "Setting up environment successful"
+fi
+
+# cleanup
+rm -Rf /oracle/install
---
+++ primary/demo.sh
@@ -0,0 +1,83 @@
+echo Database Creation started
+ORACLE_BASE=/u01/app/oracle
+ORACLE_HOME=$ORACLE_BASE/product/12.1.0
+ORACLE_SID=DEMO
+PATH=$ORACLE_HOME/bin:$ORACLE_HOME/perl/bin:$PATH;
+export ORACLE_BASE ORACLE_HOME ORACLE_SID PATH
+
+
+OLD_UMASK=`umask`
+umask 0027
+su - oracle -c "mkdir -p $ORACLE_BASE/admin/DEMO/adump"
+su - oracle -c "mkdir -p $ORACLE_BASE/admin/DEMO/dpdump"
+su - oracle -c "mkdir -p $ORACLE_BASE/oradata"
+su - oracle -c "mkdir -p $ORACLE_BASE/fast_recovery_area"
+su - oracle -c "mkdir -p $ORACLE_BASE/cfgtoollogs/dbca/DEMO"
+su - oracle -c "mkdir -p $ORACLE_HOME/dbs"
+su - oracle -c "cp /tmp/listener.ora $ORACLE_HOME/network/admin/listener.ora"
+su - oracle -c "cp /tmp/tnsnames.ora $ORACLE_HOME/network/admin/tnsnames.ora"
+su - oracle -c "cp /tmp/initDEMO.ora $ORACLE_HOME/dbs"
+
+su - oracle  -c "$ORACLE_HOME/bin/dbca \
+-silent  \
+-createDatabase \
+-templateName General_Purpose.dbc  \
+-gdbName DEMO \
+-sid DEMO  \
+-createAsContainerDatabase false  \
+-SysPassword oracle  \
+-SystemPassword oracle  \
+-datafileDestination /u01/app/oracle/oradata \
+-storageType FS  \
+-characterSet AL32UTF8  \
+-memoryPercentage 40 \
+-listeners LISTENER " 
+if [[ $? != "0" ]]; then echo "ERROR in demo.sh - aborting setup"; exit; fi
+
+su oracle -c "$ORACLE_HOME/bin/sqlplus / as sysdba << EOF
+alter system set dg_broker_start = true scope=spfile;
+shutdown immediate;
+startup mount;
+alter database archivelog;
+alter database force logging;
+alter database open;
+EOF"
+
+if [[ $? != "0" ]]; then echo "ERROR in demo.sh - aborting setup"; exit; fi
+
+
+# listener.ora & tnsnames.ora
+#echo Installing listener.ora and tnsnames.ora 
+#mkdir -p /var/opt/oracle 
+#chown oracle:oinstall /var/opt/oracle
+#chmod 766 /var/opt/oracle 
+#cp /tmp/listener.ora /var/opt/oracle
+#cp /tmp/tnsnames.ora /var/opt/oracle
+#chown oracle:oinstall /var/opt/oracle/listener.ora 
+#chown oracle:oinstall /var/opt/oracle/tnsnames.ora
+
+# start listener
+echo Starting listener 
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/12.1.0
+export TNS_ADMIN=/var/opt/oracle
+su - oracle -c "$ORACLE_HOME/bin/lsnrctl start listener"
+
+# implement oracle at boot time
+SRC=/tmp/oracle
+DEST=/etc/init.d/oracle
+cp $SRC $DEST
+chmod 0755 $DEST
+
+chkconfig oracle on
+
+
+if [[ $? != "0" ]]; then echo "ERROR in postinstall.sh - aborting setup"; exit; fi
+
+echo postinstall finished $(date) | tee -a $LOGFILE
+# ckech & cleanup
+rm -rf /var/cache/yum/x86_64 \
+/oracle/install/linuxamd64_12102_database \
+/u01/app/oracle/product/12.1.0/inventory/backup/* \
+/u01/app/oracle/product/12.1.0/assistants/dbca/templates
+
